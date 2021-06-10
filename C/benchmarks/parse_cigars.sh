16ffa34de1bbf075b59e4e20f72627f734c7fc81
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2021-06-10 08:37:24 +0200

    fix paths C examples and improve docs

--- C/benchmarks/parse_cigars.sh
+++ C/benchmarks/parse_cigars.sh
@@ -1,5 +1,16 @@
-time ./parse_cigars \
-  ../../data/cigars/cigar.datatypes.yaml \
-  cigar \
-  ../../data/cigars/100k_cigars_len100 \
-  decode > results
+#!/bin/bash
+
+cdir="../../bio/benchmarks/cigars/"
+spec="$cdir/cigar.datatypes.yaml"
+dt="cigar"
+input="$cdir/100k_cigars_len100"
+output="results"
+
+>&2 echo "### Running parse_cigars ###"
+>&2 echo "# Parameters:"
+>&2 echo "#   Specification: $spec"
+>&2 echo "#   Datatype:      $dt"
+>&2 echo "#   Input file:    $input"
+>&2 echo "#   Output file:   $output"
+>&2 echo "# ... parsing cigars ..."
+time ./parse_cigars $spec $dt $input decode > $output
--- C/examples/small_example.c
+++ C/examples/small_example.c
@@ -3,14 +3,22 @@
 
 int main(void)
 {
-  void *table, *mydef, *node;
+  void *spec, *datatype, *node;
   const char *encoded = "1M100D1I2M3M4M";
   printf("Encoded: %s\n", encoded);
+
+  /* (1) initialize Nim library (at program begin) */
   NimMain();
-  table = parse_specification("../../data/cigars/cigar.datatypes.yaml");
-  mydef = get_definition(table, "cigar");
-  node = decode((char*)encoded, mydef);
+
+  /* (2) parse specification and get datatype definition  */
+  spec = parse_specification(
+      "../../bio/benchmarks/cigars/cigar.datatypes.yaml");
+  datatype = get_definition(spec, "cigar");
+
+  /* (3) decode to a "node", convert to_string(), release memory */
+  node = decode((char*)encoded, datatype);
   printf("%s\n", to_string(node));
   GC_unref_node(node);
+
   return 0;
 }
