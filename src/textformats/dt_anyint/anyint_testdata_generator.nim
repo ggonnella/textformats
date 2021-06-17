2b20b2890b6f6c0c9212b9402752a542c52f025e
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2021-06-17 20:02:03 +0200

    use int32 in generated testdata because of limitations of YAML parsing

--- src/textformats/dt_anyint/anyint_testdata_generator.nim
+++ src/textformats/dt_anyint/anyint_testdata_generator.nim
@@ -5,8 +5,11 @@ import ../types / [datatype_definition, testdata]
 import ../support / [openrange]
 import ../shared/num_testdata_generator
 
+#
+# Note: using int32 because of limitations of the YAML parser
+#
 proc add_valid_anyint(t: var TestData) =
-  let values = @[0, 1, -1, int.high, int.low]
+  let values = @[0, 1, -1, int32.high, int32.low]
   for v in values: t.v[$v] = %*v
 
 proc anyint_generate_testdata*(t: var Testdata, dd: DatatypeDefinition) =
--- src/textformats/dt_anyuint/anyuint_testdata_generator.nim
+++ src/textformats/dt_anyuint/anyuint_testdata_generator.nim
@@ -5,8 +5,11 @@ import ../types / [datatype_definition, testdata]
 import ../support / [openrange]
 import ../shared/num_testdata_generator
 
+#
+# Note: using int32 because of limitations of the YAML parser
+#
 proc add_valid_anyuint(t: var TestData) =
-  let values = @[0, 1, int.high]
+  let values = @[0, 1, int32.high]
   for v in values: t.v[$v] = %*v
 
 proc anyuint_generate_testdata*(t: var Testdata, dd: DatatypeDefinition) =
