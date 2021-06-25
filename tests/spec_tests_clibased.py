162ece6405c526886615f820113546283e1d9f53
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2021-06-25 15:15:49 +0200

    spec_tests_clibased script, remove use of -p

--- tests/spec_tests_clibased.py
+++ tests/spec_tests_clibased.py
@@ -58,66 +58,56 @@ test_validate_encoded = partial(textformats, "encoded_validation", _fg=True)
 test_decode_invalid = partial(textformats, "fail_decoding", _fg=True)
 test_encode_invalid = partial(textformats, "fail_encoding", _fg=True)
 
-def test_valid_encoded(specfile, preprocessed, datatype, encoded, decoded):
-  test_decode(s=specfile, p=preprocessed, t=datatype, e=encoded, d=decoded)
-  test_validate_encoded(s=specfile, p=preprocessed, t=datatype, e=encoded,
-                        v=True)
+def test_valid_encoded(specfile, datatype, encoded, decoded):
+  test_decode(s=specfile, t=datatype, e=encoded, d=decoded)
+  test_validate_encoded(s=specfile, t=datatype, e=encoded, v=True)
 
-def test_valid_decoded(specfile, preprocessed, datatype, encoded, decoded):
-  test_encode(s=specfile, p=preprocessed, t=datatype, e=encoded, d=decoded)
-  test_validate_decoded(s=specfile, p=preprocessed, t=datatype, d=decoded,
-                        v=True)
+def test_valid_decoded(specfile, datatype, encoded, decoded):
+  test_encode(s=specfile, t=datatype, e=encoded, d=decoded)
+  test_validate_decoded(s=specfile, t=datatype, d=decoded, v=True)
 
-def test_invalid_encoded(specfile, preprocessed, datatype, encoded):
-  test_decode_invalid(s=specfile, p=preprocessed, t=datatype, e=encoded)
-  test_validate_encoded(s=specfile, p=preprocessed, t=datatype, e=encoded,
-                        v=False)
+def test_invalid_encoded(specfile, datatype, encoded):
+  test_decode_invalid(s=specfile, t=datatype, e=encoded)
+  test_validate_encoded(s=specfile, t=datatype, e=encoded, v=False)
 
-def test_invalid_decoded(specfile, preprocessed, datatype, decoded):
-  test_encode_invalid(s=specfile, p=preprocessed, t=datatype, d=decoded)
-  test_validate_decoded(s=specfile, p=preprocessed, t=datatype, d=decoded,
-                        v=False)
+def test_invalid_decoded(specfile, datatype, decoded):
+  test_encode_invalid(s=specfile, t=datatype, d=decoded)
+  test_validate_decoded(s=specfile, t=datatype, d=decoded, v=False)
 
 def get_as_list(d, key):
   result = d.get(key, [])
   return result if isinstance(result, list) else [result]
 
-def test_all_invalid(specfile, preprocessed, datatype, tests):
+def test_all_invalid(specfile, datatype, tests):
   key = "invalid"
   if key in tests:
     if isinstance(tests[key], dict):
       for encoded in get_as_list(tests[key], "encoded"):
-        test_invalid_encoded(specfile, preprocessed, datatype, encoded)
+        test_invalid_encoded(specfile, datatype, encoded)
       for decoded in get_as_list(tests[key], "decoded"):
-        test_invalid_decoded(specfile, preprocessed, datatype,
-                             json.dumps(decoded))
+        test_invalid_decoded(specfile, datatype, json.dumps(decoded))
     else:
       for item in get_as_list(tests, key):
-        test_invalid_encoded(specfile, preprocessed, datatype, item)
-        test_invalid_decoded(specfile, preprocessed, datatype, json.dumps(item))
+        test_invalid_encoded(specfile, datatype, item)
+        test_invalid_decoded(specfile, datatype, json.dumps(item))
 
-def test_all_valid(specfile, preprocessed, datatype, tests):
+def test_all_valid(specfile, datatype, tests):
   key = "valid"
   if key in tests:
     if isinstance(tests[key], dict):
       for encoded, decoded in tests[key].items():
-        test_valid_encoded(specfile, preprocessed, datatype, encoded,
-                           json.dumps(decoded))
-        test_valid_decoded(specfile, preprocessed, datatype, encoded,
-                           json.dumps(decoded))
+        test_valid_encoded(specfile, datatype, encoded, json.dumps(decoded))
+        test_valid_decoded(specfile, datatype, encoded, json.dumps(decoded))
     else:
       for item in get_as_list(tests, key):
-        test_valid_encoded(specfile, preprocessed, datatype,
-                           item, json.dumps(item))
-        test_valid_decoded(specfile, preprocessed, datatype,
-                           item, json.dumps(item))
+        test_valid_encoded(specfile, datatype, item, json.dumps(item))
+        test_valid_decoded(specfile, datatype, item, json.dumps(item))
 
-def test_all_secondary(specfile, preprocessed, datatype, tests):
+def test_all_secondary(specfile, datatype, tests):
   key = "oneway"
   if key in tests:
     for encoded, decoded in tests[key].items():
-      test_valid_encoded(specfile, preprocessed, datatype, encoded,
-                         json.dumps(decoded))
+      test_valid_encoded(specfile, datatype, encoded, json.dumps(decoded))
 
 def main(arguments):
   specfile = arguments["<spec>"]
@@ -134,9 +124,9 @@ def main(arguments):
       # in an embedded spec, thus skip it
       break
     for datatype, tests in data[mainkey].items():
-      test_all_valid(specfile, preprocessed, datatype, tests)
-      test_all_secondary(specfile, preprocessed, datatype, tests)
-      test_all_invalid(specfile, preprocessed, datatype, tests)
+      test_all_valid(specfile, datatype, tests)
+      test_all_secondary(specfile, datatype, tests)
+      test_all_invalid(specfile, datatype, tests)
 
 if __name__ == "__main__":
   arguments = docopt.docopt(__doc__, version="0.1")
