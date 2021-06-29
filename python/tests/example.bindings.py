7e44c0848fff4fb8fe043758a5ba3c73673bcb0d
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2021-06-29 17:32:28 +0200

    start completing python bindings for all API functions

--- python/tests/example.bindings.py
+++ python/tests/example.bindings.py
@@ -4,13 +4,51 @@ import nimporter
 from textformats.py_bindings import *
 
 testdir = os.path.dirname(os.path.realpath(__file__))
-specfile = testdir + "/../../bio/spec/gfa2/gfa2.datatypes.yaml"
-spec = parse_specification(specfile)
-gfa2_position = get_definition(spec, "gfa2_position")
+gfa2_specfile = testdir + "/../../bio/spec/gfa/gfa2.yaml"
+gfa2_pspecfile = testdir + "/gfa2.tfs"
+gfa2_datafile = testdir + "/../../bio/data/test.gfa2"
+fasta_specfile = testdir + "/../../bio/spec/fasta.yaml"
+fasta_datafile = testdir + "/../../bio/data/test.fas"
+fastq_specfile = testdir + "/../../bio/spec/fastq.yaml"
+fastq_datafile = testdir + "/../../bio/data/test.fq"
+
+preprocess_specification(gfa2_specfile, gfa2_pspecfile)
+print(is_preprocessed(gfa2_specfile))
+print(is_preprocessed(gfa2_pspecfile))
+gfa2_spec = specification_from_file(gfa2_specfile)
+
+gfa2_position = get_definition(gfa2_spec, "gfa2::position")
+print(describe(gfa2_position))
+
 decoded = decode("2$", gfa2_position)
 print(decoded)
 print(is_valid_decoded(decoded, gfa2_position))
+
 encoded = encode(decoded, gfa2_position)
 print(encoded)
 print(is_valid_encoded(encoded, gfa2_position))
 
+#gfa2_line = get_definition(gfa2_spec, "line")
+#for line in decoded_lines(gfa2_datafile, gfa2_line):
+#  print(line)
+#
+fasta_spec = specification_from_file(fasta_specfile)
+fasta_entry = get_definition(fasta_spec, "entry")
+#for section in decoded_sections(fasta_datafile, fasta_entry):
+#  print(section)
+#for section in decoded_sections(fasta_specfile, fasta_entry,
+#                                     embedded=True):
+#  print(section)
+#
+#fastq_spec = specification_from_file(fastq_specfile)
+#fastq_entry = get_definition(fastq_spec, "fastq_entry")
+#for unit in decoded_units(fastq_datafile, fastq_entry, 4):
+#  print(unit)
+#for unit in decoded_units(fastq_specfile, fastq_entry, 4, True):
+#  print(unit)
+
+def line_processor(decoded):
+  print(f"DECODED: {decoded}")
+
+decode_section_lines(fasta_specfile, fasta_entry,
+                     line_processor, False, True)
--- python/textformats/py_bindings/py_bindings.nim
+++ python/textformats/py_bindings/py_bindings.nim
@@ -1,37 +1,54 @@
-import nimpy
+import nimpy, os
 from json import JsonNode, `$`, parse_json
 from textformats import Specification, DatatypeDefinition
 from textformats import nil
 
 proc get_definition*(datatypes: Specification,
-                     datatype: string):
+                     datatype: string = "default"):
                      DatatypeDefinition {.exportpy.} =
   textformats.get_definition(datatypes, datatype)
 
-proc parse_specification*(filename: string):
-                                 Specification {.exportpy.} =
-  textformats.parse_specification(filename)
+proc describe*(dd: DatatypeDefinition): string {.exportpy.} =
+  textformats.`$`(dd)
+
+proc specification_from_file*(filename: string):
+                              Specification {.exportpy.} =
+  if not fileExists(filename):
+    raise newException(textformats.TextformatsRuntimeError,
+                       "File not found:" & filename)
+  textformats.specification_from_file(filename)
+
+proc preprocess_specification*(inputfile: string, outputfile: string)
+                              {.exportpy.} =
+  if not fileExists(inputfile):
+    raise newException(textformats.TextformatsRuntimeError,
+                       "File not found:" & inputfile)
+  textformats.preprocess_specification(inputfile, outputfile)
+
+proc is_preprocessed*(filename: string): bool {.exportpy.} =
+  if not fileExists(filename):
+    raise newException(textformats.TextformatsRuntimeError,
+                       "File not found:" & filename)
+  textformats.is_preprocessed(filename)
 
 proc decode*(input: string, dd: DatatypeDefinition): JsonNode {.exportpy.} =
   textformats.decode(input, dd)
 
-proc to_json*(input: string, dd: DatatypeDefinition):
-                     string {.exportpy.} =
+proc to_json*(input: string, dd: DatatypeDefinition): string {.exportpy.} =
   $textformats.decode(input, dd)
 
 proc encode*(obj: PyObject, dd: DatatypeDefinition): string {.exportpy.} =
   textformats.encode(obj.to_json, dd)
 
-proc unsafe_encode*(obj: PyObject, dd: DatatypeDefinition):
-                                                     string {.exportpy.} =
+proc unsafe_encode*(obj: PyObject,
+                    dd: DatatypeDefinition): string {.exportpy.} =
   textformats.unsafe_encode(obj.to_json, dd)
 
-proc from_json*(json_str: string, dd: DatatypeDefinition):
-                      string {.exportpy.} =
+proc from_json*(json_str: string, dd: DatatypeDefinition): string {.exportpy.} =
   textformats.encode(parse_json(json_str), dd)
 
-proc unsafe_from_json*(json_str: string, dd: DatatypeDefinition):
-                      string {.exportpy.} =
+proc unsafe_from_json*(json_str: string,
+                       dd: DatatypeDefinition): string {.exportpy.} =
   textformats.unsafe_encode(parse_json(json_str), dd)
 
 proc is_valid_encoded*(
@@ -41,3 +58,25 @@ proc is_valid_encoded*(
 proc is_valid_decoded*(
        obj: PyObject, dd: DatatypeDefinition): bool {.exportpy.} =
   textformats.is_valid(obj.to_json, dd)
+
+iterator decoded_lines*(filename: string, dd: DatatypeDefinition,
+                       embedded: bool = false, wrapped: bool = false):
+                         JsonNode {.exportpy.} =
+  for decoded in textformats.decoded_lines(filename, dd, embedded, wrapped):
+    yield decoded
+
+iterator decoded_units*(filename: string, dd: DatatypeDefinition, unitsize: int,
+                        embedded: bool = false, wrapped: bool = false):
+                         JsonNode {.exportpy.} =
+  for decoded in textformats.decoded_units(filename, dd, unitsize,
+                                           embedded, wrapped):
+    yield decoded
+
+iterator decoded_sections*(filename: string, dd: DatatypeDefinition,
+                           embedded: bool = false): JsonNode {.exportpy.} =
+  for decoded in textformats.decoded_sections(filename, dd, embedded):
+    yield decoded
+
+##export decode_file
+##export decode_file_section_lines
+##export test_specification
