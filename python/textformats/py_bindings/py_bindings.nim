dc85a03365673ba3e52356b9bf24b9924547f325
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2021-07-06 16:37:06 +0200

    Python, move examples under examples, make place for systematic tests

--- python/textformats/py_bindings/py_bindings.nim
+++ python/textformats/py_bindings/py_bindings.nim
@@ -1,3 +1,12 @@
+##
+## This module shall be considered private. The public Python API
+## is implemented in the textformats Python module (which imports this module
+## using nimporter).
+##
+## This module defines functions which wrap the Nim API by using nimpy and
+## the {.exportpy.} pragma. Furthermore JsonNode parameters and return values
+## are converted to/from PyObject.
+##
 import nimpy, os
 from json import JsonNode, `$`, parse_json
 from textformats import Specification, DatatypeDefinition
@@ -31,25 +40,25 @@ proc is_preprocessed*(filename: string): bool {.exportpy.} =
                        "File not found:" & filename)
   textformats.is_preprocessed(filename)
 
+proc test_specification(spec: Specification, testfile: string) {.exportpy.} =
+  textformats.test_specification(spec, testfile)
+
+proc datatype_names(spec: Specification): seq[string] {.exportpy.} =
+  textformats.datatype_names(spec)
+
 proc decode*(input: string, dd: DatatypeDefinition): JsonNode {.exportpy.} =
   textformats.decode(input, dd)
 
-proc to_json*(input: string, dd: DatatypeDefinition): string {.exportpy.} =
+proc decode_to_json*(input: string, dd: DatatypeDefinition):
+                    string {.exportpy.} =
   $textformats.decode(input, dd)
 
 proc encode*(obj: PyObject, dd: DatatypeDefinition): string {.exportpy.} =
   textformats.encode(obj.to_json, dd)
 
-proc unsafe_encode*(obj: PyObject,
-                    dd: DatatypeDefinition): string {.exportpy.} =
-  textformats.unsafe_encode(obj.to_json, dd)
-
-proc from_json*(json_str: string, dd: DatatypeDefinition): string {.exportpy.} =
+proc encode_json*(json_str: string, dd: DatatypeDefinition):
+                  string {.exportpy.} =
   textformats.encode(parse_json(json_str), dd)
-
-proc unsafe_from_json*(json_str: string,
-                       dd: DatatypeDefinition): string {.exportpy.} =
-  textformats.unsafe_encode(parse_json(json_str), dd)
 
 proc is_valid_encoded*(
        input: string, dd: DatatypeDefinition): bool {.exportpy.} =
@@ -63,30 +72,28 @@ proc is_valid_decoded_json*(
        json_str: string, dd: DatatypeDefinition): bool {.exportpy.} =
   textformats.is_valid(parse_json(json_str), dd)
 
-iterator decoded_file_values*(filename: string, dd: DatatypeDefinition,
-                  embedded: bool = false, scope: string = "auto",
-                  elemwise: bool = false, wrapped: bool = false,
-                  unitsize: int = 1): JsonNode {.exportpy.} =
-  for decoded in textformats.decoded_file_values(filename, dd, embedded, scope,
-                                                 elemwise, wrapped, unitsize):
+iterator decoded_file*(filename: string, dd: DatatypeDefinition,
+                  embedded: bool = false, splitted: bool = false,
+                  wrapped: bool = false): JsonNode {.exportpy.} =
+  for decoded in textformats.decoded_file(filename, dd, embedded,
+                                          splitted, wrapped):
     yield decoded
 
-iterator file_values_to_json*(filename: string, dd: DatatypeDefinition,
-                  embedded: bool = false, scope: string = "auto",
-                  elemwise: bool = false, wrapped: bool = false,
-                  unitsize: int = 1): string {.exportpy.} =
-  for decoded in textformats.decoded_file_values(filename, dd, embedded, scope,
-                                                 elemwise, wrapped, unitsize):
+iterator decoded_file_as_json*(filename: string, dd: DatatypeDefinition,
+                  embedded: bool = false, splitted: bool = false,
+                  wrapped: bool = false): JsonNode {.exportpy.} =
+  for decoded in textformats.decoded_file(filename, dd, embedded,
+                                          splitted, wrapped)
     yield $decoded
-
-proc test_specification(spec: Specification, testfile: string) {.exportpy.} =
-  textformats.test_specification(spec, testfile)
-
-proc datatype_names(spec: Specification): seq[string] {.exportpy.} =
-  textformats.datatype_names(spec)
 
 proc get_unitsize(dd: DatatypeDefinition): int {.exportpy.} =
   textformats.get_unitsize(dd)
 
 proc get_scope(dd: DatatypeDefinition): string {.exportpy.} =
   textformats.get_scope(dd)
+
+proc set_unitsize(dd: DatatypeDefinition, unitsize: int) {.exportpy.} =
+  textformats.set_unitsize(dd, unitsize)
+
+proc set_scope(dd: DatatypeDefinition, scope: string) {.exportpy.} =
+  textformats.set_scope(dd, scope)
--- python/textformats/textformats.py
+++ python/textformats/textformats.py
@@ -5,78 +5,41 @@ class Datatype:
   def __init__(self, definition, reprstr="Datatype"):
     self._definition = definition
     self._reprstr = reprstr
-    self._scope = tf.get_scope(definition)
-    self._unitsize = tf.get_unitsize(definition)
 
   def decode(self, encoded, to_json=False):
-    if to_json: return tf.to_json(encoded, self._definition)
+    if to_json: return tf.decode_to_json(encoded, self._definition)
     else: return tf.decode(encoded, self._definition)
-
-  ValidScopes = ["line", "unit", "section", "file", "auto"]
 
   @property
   def scope(self):
-    return self._scope
+    return tf.get_scope(self._definition)
 
   @scope.setter
   def scope(self, value):
-    if value not in Datatype.ValidScopes:
-      raise f"Error: scope must be one of {', '.join(Datatype.ValidScopes)}"
-    if value == "auto":
-      self._scope = tf.get_scope(self._definition)
-      if self._scope == "undefined":
-        raise "Error: setting scope to auto, i.e. to the value set " +\
-              "in the 'scope' key of the datatype definition, " +\
-              "but no value is set in the definition"
-    else:
-      self._scope = value
+    tf.set_scope(self._definition, value)
 
   @property
   def unitsize(self):
-    return self._unitsize
+    return tf.get_scope(self._definition)
 
   @unitsize.setter
   def unitsize(self, value):
-    if value == "auto":
-      self._unitsize = tf.get_unitsize(self._definition)
-    elif not isinstance(value, int) or value < 1:
-      raise "Error: the unitsize property must be set to 'auto', i.e. to "+\
-            "the value set in the 'unitsize' key of the datatype definition "+\
-            "or to an integer value > 1"
-    else:
-      self._unitsize = value
+    tf.set_unitsize(self._definition, value)
 
   def decoded_file(self, filename, embedded=False, splitted=False,
-                          wrapped=False, to_json=False):
-    if self.scope == "undefined":
-      raise "Error: undefined scope\n" +\
-            "Hint: set the Datatype scope either using the 'scope' key " +\
-            "of the datatype definition, or setting the scope property of " +\
-            "this object"
-    elif self.scope == "unit":
-      if self.unitsize <= 1:
-        raise f"Error: unitsize value invalid ({self.unitsize})\n" +\
-              "set the Datatype unitsize to a value > 1 either using "+\
-              "the 'unitsize' key of the datatype definition, or "+\
-              "setting the unitsize property of this object"
+                   wrapped=False, to_json=False):
     if to_json:
-      for value in tf.file_values_to_json(filename, self._definition, embedded,
-                                       self.scope, splitted, wrapped,
-                                       self.unitsize):
+      for value in tf.decoded_file_as_json(filename, self._definition, embedded,
+                                           splitted, wrapped):
         yield value
     else:
-      for value in tf.decoded_file_values(filename, self._definition, embedded,
-                                       self.scope, splitted, wrapped,
-                                       self.unitsize):
+      for value in tf.decoded_file(filename, self._definition, embedded,
+                                   splitted, wrapped):
         yield value
 
-  def encode(self, item, from_json=False, unsafe=False):
-    if unsafe:
-      if from_json: return tf.unsafe_from_json(item, self._definition)
-      else: return tf.unsafe_encode(item, self._definition)
-    else:
-      if from_json: return tf.from_json(item, self._definition)
-      else: return tf.encode(item, self._definition)
+  def encode(self, item, from_json=False):
+    if from_json: return tf.encode_json(item, self._definition)
+    else: return tf.encode(item, self._definition)
 
   def is_valid_decoded(self, s, json=False):
     if json: return tf.is_valid_decoded(s, self._definition)
