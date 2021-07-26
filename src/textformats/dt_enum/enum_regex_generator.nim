6fe41d11a2d698b1b7012e5fb09d2cf6788f71d5
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2021-07-26 16:23:52 +0300

    improv enum regex for single chars wo translation map

--- src/textformats/dt_enum/enum_regex_generator.nim
+++ src/textformats/dt_enum/enum_regex_generator.nim
@@ -12,23 +12,35 @@ proc enum_compute_regex*(dd: DatatypeDefinition) =
     float_added = false
     regexes = newseq[string](dd.elements.len)
   dd.regex.ensures_valid = true
-  var has_translations = false
+  var
+    has_translations = false
+    charclass = true
   for i, e in dd.elements:
     if dd.decoded[i].is_some:
       has_translations = true
     case e.kind:
       of meString:
-        regexes[i] = e.s_value.escape_re
+        let rgx = e.s_value.escape_re
+        if len(rgx) > 1:
+          charclass = false
+        regexes[i] = rgx
       of meFloat:
         if float_added:
           continue
         else:
           regexes[i] = AnyFloatRE
           dd.regex.ensures_valid = false
+          charclass = false
       of meInt:
         let i_str = if i > 0: "\\+?" & $i else: $i
         regexes[i] = i_str
+        charclass = false
   if has_translations:
     for i in 0..<dd.elements.len:
       regexes[i] = regexes[i].to_named_group($i)
-  dd.regex.raw = "(" & regexes.join("|") & ")"
+    dd.regex.raw = "(" & regexes.join("|") & ")"
+  else:
+    if charclass:
+      dd.regex.raw = "[" & regexes.join("") & "]"
+    else:
+      dd.regex.raw = "(" & regexes.join("|") & ")"
