e0766dc207694b2dab7d7fef071bc1adc25f8924
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2022-02-15 11:17:15 +0100

    also Py API source distributions wo nimporter

--- python/setup.py
+++ python/setup.py
@@ -1,39 +1,80 @@
-from setuptools import setup
-import sys
-if not sys.version_info[0] == 3:
-  sys.exit("Sorry, only Python 3 is supported")
+from setuptools import setup, Extension
+from glob import glob
+import subprocess
+import re
+import os
+import shutil
 
-def readme():
-  with open('README.md') as f:
-    return f.read()
 
-setup(name='textformats',
-      packages=["textformats"],
-      package_data={"": ['*.so']},
-      version='1.2.0',
-      description='Easily defined compact human-readable data representations',
-      long_description=readme(),
-      long_description_content_type='text/markdown',
-      url='https://github.com/ggonnella/textformats',
-      keywords="bioinformatics genomics sequences GFA assembly graphs",
-      author='Giorgio Gonnella',
-      author_email='gonnella@zbh.uni-hamburg.de',
-      license='ISC',
-      # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Software Development :: Libraries',
-      ],
-      scripts=[],
-      zip_safe=False,
-      test_suite="nose.collector",
-      tests_require=['nose'],
+errmsg="""
+
+---------------------------------------------------------------------
+
+PROBLEM:
+
+For installing a source extension developed in Nim, the Nim library
+is necessary. However, it could not be located in the system.
+
+SOLUTION:
+
+Is the Nim compiler already installed?
+
+[Yes] => provide the Nim compiler location as follows:
+
+           env NIM=/path/to/nim pip install textformats
+
+[No] => install Nim, using one of the following systems
+        and run 'pip install textformats' afterwards
+
+  [pip]        pip install choosenim_install
+
+  [conda]      conda install nim
+
+  [choosenim]  curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+
+  [pre-compiled nim] see: https://nim-lang.org/install_unix.html
+
+---------------------------------------------------------------------
+
+"""
+
+def find_nimlib():
+  nim_bin = os.environ.get("NIM", None)
+  if nim_bin:
+    print(f"NIM env variable found, value: {nim_bin}")
+  else:
+    print(f"NIM env variable not found")
+    nim_bin = shutil.which("nim")
+    if nim_bin:
+      print(f"'nim' found in PATH: {nim_bin}")
+    else:
+      print(f"'nim' not found in PATH: {os.get_exec_path()}")
+      raise RuntimeError(errmsg)
+  if shutil.which("choosenim"):
+    proc = subprocess.Popen(["nim", "--version"], stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    nim_ver = re.findall(r"Version (\S+)", stdout.decode("utf-8"))[0]
+    nim_dir=os.path.join(os.environ["HOME"], ".choosenim", "toolchains",
+                         f"nim-{nim_ver}")
+  else:
+    nim_dir = os.path.dirname(os.path.dirname(
+                os.path.abspath(os.path.realpath(nim_bin))))
+  nim_lib = os.path.join(nim_dir, "lib")
+  if not os.path.exists(os.path.join(nim_lib,"nimbase.h")):
+    raise RuntimeError(f"Could not find nim library header in {nim_lib}\n\n"+
+                       errmsg)
+  return nim_lib
+
+setup_args = dict(
+  ext_modules = [
+    Extension(
+      'textformats.py_bindings',
+      glob("py_bindings/extension/*.c"),
+      include_dirs = [find_nimlib()],
+      py_limited_api = True,
+      extra_compile_args=["-w"]
     )
+  ]
+)
+setup(**setup_args)
